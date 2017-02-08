c45b25267ff1684ad2e480b68dea799836aa4ad8
Administrador <Administrador@su1-01.uninorteacad.uninorte.edu.co>
2017-02-08 15:46:51 -0500

    programacion orientada a objeto

--- src/universidad/Curso.java
+++ src/universidad/Curso.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidad;
 
-/**
- *
- * @author Administrador
- */
+
 public class Curso {
     private int nrc;
     private Profesor profesor;
--- src/universidad/Profesor.java
+++ src/universidad/Profesor.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidad;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Administrador
- */
+
 public class Profesor {
     
     int id;
